MODAODF/odfweb_grouprepos

實作使用者資料夾顯示捷徑
使用者資料夾顯示捷徑:升級至新版list

lib/AppInfo/Application.php
@@ -196,6 +196,15 @@ class Application extends App implements IBootstrap {
 			});
 			$cacheListener->listen();
 		});
+
+		\OCA\Files\App::getNavigationManager()->add([
+			'id' => 'groupreposslist',
+			'appname' => 'grouprepos',
+			'script' => 'list.php',
+			'order' => 25,
+			'name' => "群組資料夾",
+			'icon' => "grouprepos"
+		]);
 	}
 
 	public function getMountProvider(): MountProvider {

MODAODF/odfweb_mergeodf

lib/AppInfo/Application.php
@@ -196,6 +196,15 @@ class Application extends App implements IBootstrap {
 			});
 			$cacheListener->listen();
 		});
+
+		\OCA\Files\App::getNavigationManager()->add([
+			'id' => 'groupfolderslist',
+			'appname' => 'groupfolders',
+			'script' => 'list.php',
+			'order' => 25,
+			'name' => "群組資料夾",
+			'icon' => "groupfolders"
+		]);
 	}
 
 	public function getMountProvider(): MountProvider {