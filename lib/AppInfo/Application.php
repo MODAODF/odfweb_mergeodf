nextcloud/groupfolders

allow enabling encryption for groupfolders
Signed-off-by: Robin Appelman <robin@icewind.nl>

lib/AppInfo/Application.php
@@ -78,6 +78,7 @@ class Application extends App implements IBootstrap {
 			};
 			$config = $c->get(IConfig::class);
 			$allowRootShare = $config->getAppValue('groupfolders', 'allow_root_share', 'true') === 'true';
+			$enableEncryption = $config->getAppValue('groupfolders', 'enable_encryption', 'false') === 'true';
 
 			return new MountProvider(
 				$c->getServer()->getGroupManager(),
@@ -90,7 +91,8 @@ class Application extends App implements IBootstrap {
 				$c->get(IMountProviderCollection::class),
 				$c->get(IDBConnection::class),
 				$c->get(ICacheFactory::class)->createLocal("groupfolders"),
-				$allowRootShare
+				$allowRootShare,
+				$enableEncryption
 			);
 		});
 

lib/Mount/GroupFolderNoEncryptionStorage.php
@@ -1,6 +1,8 @@
 <?php
+
+declare(strict_types=1);
 /**
- * @copyright Copyright (c) 2017 Robin Appelman <robin@icewind.nl>
+ * @copyright Copyright (c) 2022 Robin Appelman <robin@icewind.nl>
  *
  * @license GNU AGPL version 3 or any later version
  *
@@ -21,26 +23,7 @@
 
 namespace OCA\GroupFolders\Mount;
 
-use OC\Files\Mount\MountPoint;
+use OCP\Files\Storage\IDisableEncryptionStorage;
 
-class GroupMountPoint extends MountPoint {
-	/** @var int */
-	private $folderId;
-
-	public function __construct($folderId, $storage, $mountpoint, $arguments = null, $loader = null, $mountOptions = null, $mountId = null) {
-		$this->folderId = $folderId;
-		parent::__construct($storage, $mountpoint, $arguments, $loader, $mountOptions, $mountId, MountProvider::class);
-	}
-
-	public function getMountType() {
-		return 'group';
-	}
-
-	public function getFolderId(): int {
-		return $this->folderId;
-	}
-
-	public function getSourcePath(): string {
-		return '/__groupfolders/' . $this->getFolderId();
-	}
+class GroupFolderNoEncryptionStorage extends GroupFolderStorage implements IDisableEncryptionStorage {
 }

lib/Mount/MountProvider.php
@@ -71,6 +71,7 @@ class MountProvider implements IMountProvider {
 	private ICache $cache;
 	private ?int $rootStorageId = null;
 	private bool $allowRootShare;
+	private bool $enableEncryption;
 
 	public function __construct(
 		IGroupManager $groupProvider,
@@ -83,7 +84,8 @@ class MountProvider implements IMountProvider {
 		IMountProviderCollection $mountProviderCollection,
 		IDBConnection $connection,
 		ICache $cache,
-		bool $allowRootShare
+		bool $allowRootShare,
+		bool $enableEncryption
 	) {
 		$this->groupProvider = $groupProvider;
 		$this->folderManager = $folderManager;
@@ -96,6 +98,7 @@ class MountProvider implements IMountProvider {
 		$this->connection = $connection;
 		$this->cache = $cache;
 		$this->allowRootShare = $allowRootShare;
+		$this->enableEncryption = $enableEncryption;
 	}
 
 	private function getRootStorageId(): int {
@@ -208,14 +211,25 @@ class MountProvider implements IMountProvider {
 			'storage' => $storage,
 			'root' => $rootPath
 		]);
-		$quotaStorage = new GroupFolderStorage([
-			'storage' => $baseStorage,
-			'quota' => $quota,
-			'folder_id' => $id,
-			'rootCacheEntry' => $cacheEntry,
-			'userSession' => $this->userSession,
-			'mountOwner' => $user,
-		]);
+		if ($this->enableEncryption) {
+			$quotaStorage = new GroupFolderStorage([
+				'storage' => $baseStorage,
+				'quota' => $quota,
+				'folder_id' => $id,
+				'rootCacheEntry' => $cacheEntry,
+				'userSession' => $this->userSession,
+				'mountOwner' => $user,
+			]);
+		} else {
+			$quotaStorage = new GroupFolderNoEncryptionStorage([
+				'storage' => $baseStorage,
+				'quota' => $quota,
+				'folder_id' => $id,
+				'rootCacheEntry' => $cacheEntry,
+				'userSession' => $this->userSession,
+				'mountOwner' => $user,
+			]);
+		}
 		$maskedStore = new PermissionsMask([
 			'storage' => $quotaStorage,
 			'mask' => $permissions

MODAODF/odfweb_mergeodf

lib/AppInfo/Application.php
@@ -97,6 +97,7 @@ class Application extends App implements IBootstrap {
 			};
 			$config = $c->get(IConfig::class);
 			$allowRootShare = $config->getAppValue('mergeodf', 'allow_root_share', 'true') === 'true';
+			$enableEncryption = $config->getAppValue('mergeodf', 'enable_encryption', 'false') === 'true';
 
 			return new MountProvider(
 				$c->getServer()->getGroupManager(),
@@ -109,7 +110,8 @@ class Application extends App implements IBootstrap {
 				$c->get(IMountProviderCollection::class),
 				$c->get(IDBConnection::class),
 				$c->get(ICacheFactory::class)->createLocal("mergeodf"),
-				$allowRootShare
+				$allowRootShare,
+				$enableEncryption
 			);
 		});
 

lib/Mount/GroupFolderNoEncryptionStorage.php
@@ -1,6 +1,8 @@
 <?php
+
+declare(strict_types=1);
 /**
- * @copyright Copyright (c) 2017 Robin Appelman <robin@icewind.nl>
+ * @copyright Copyright (c) 2022 Robin Appelman <robin@icewind.nl>
  *
  * @license GNU AGPL version 3 or any later version
  *
@@ -21,26 +23,7 @@
 
 namespace OCA\MergeODF\Mount;
 
-use OC\Files\Mount\MountPoint;
+use OCP\Files\Storage\IDisableEncryptionStorage;
 
-class GroupMountPoint extends MountPoint {
-	/** @var int */
-	private $folderId;
-
-	public function __construct($folderId, $storage, $mountpoint, $arguments = null, $loader = null, $mountOptions = null, $mountId = null) {
-		$this->folderId = $folderId;
-		parent::__construct($storage, $mountpoint, $arguments, $loader, $mountOptions, $mountId, MountProvider::class);
-	}
-
-	public function getMountType() {
-		return 'mergeodf';
-	}
-
-	public function getFolderId(): int {
-		return $this->folderId;
-	}
-
-	public function getSourcePath(): string {
-		return '/__mergeodf/' . $this->getFolderId();
-	}
+class MergeODFNoEncryptionStorage extends MergeODFStorage implements IDisableEncryptionStorage {
 }

lib/Mount/MountProvider.php
@@ -71,6 +71,7 @@ class MountProvider implements IMountProvider {
 	private ICache $cache;
 	private ?int $rootStorageId = null;
 	private bool $allowRootShare;
+	private bool $enableEncryption;
 
 	public function __construct(
 		IGroupManager $groupProvider,
@@ -83,7 +84,8 @@ class MountProvider implements IMountProvider {
 		IMountProviderCollection $mountProviderCollection,
 		IDBConnection $connection,
 		ICache $cache,
-		bool $allowRootShare
+		bool $allowRootShare,
+		bool $enableEncryption
 	) {
 		$this->groupProvider = $groupProvider;
 		$this->folderManager = $folderManager;
@@ -96,6 +98,7 @@ class MountProvider implements IMountProvider {
 		$this->connection = $connection;
 		$this->cache = $cache;
 		$this->allowRootShare = $allowRootShare;
+		$this->enableEncryption = $enableEncryption;
 	}
 
 	private function getRootStorageId(): int {
@@ -208,14 +211,25 @@ class MountProvider implements IMountProvider {
 			'storage' => $storage,
 			'root' => $rootPath
 		]);
-		$quotaStorage = new MergeODFStorage([
-			'storage' => $baseStorage,
-			'quota' => $quota,
-			'folder_id' => $id,
-			'rootCacheEntry' => $cacheEntry,
-			'userSession' => $this->userSession,
-			'mountOwner' => $user,
-		]);
+		if ($this->enableEncryption) {
+			$quotaStorage = new MergeODFStorage([
+				'storage' => $baseStorage,
+				'quota' => $quota,
+				'folder_id' => $id,
+				'rootCacheEntry' => $cacheEntry,
+				'userSession' => $this->userSession,
+				'mountOwner' => $user,
+			]);
+		} else {
+			$quotaStorage = new MergeODFNoEncryptionStorage([
+				'storage' => $baseStorage,
+				'quota' => $quota,
+				'folder_id' => $id,
+				'rootCacheEntry' => $cacheEntry,
+				'userSession' => $this->userSession,
+				'mountOwner' => $user,
+			]);
+		}
 		$maskedStore = new PermissionsMask([
 			'storage' => $quotaStorage,
 			'mask' => $permissions